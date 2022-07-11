9aba4f0c556acb24a95df0daf409145682bbec19
cy22-y <64590604+cy22-y@users.noreply.github.com>
2022-07-11 23:54:18 +0800

    Delete qunar directory

--- qunar/src/main/java/org/example/question4/Commonds.java
+++
@@ -1,8 +0,0 @@
-package org.example.question4;
-
-public class Commonds {
-    public static final String CAT_COMMAND = "cat";
-    public static final String GREP_COMMAND = "grep";
-    public static final String WC_COMMAND = "wc";
-
-}
